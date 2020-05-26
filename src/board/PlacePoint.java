1623b80e343da9485cdbdaa385a2f0be10f38663
MickDog <56132560+totallyReal123@users.noreply.github.com>
2020-05-26 12:55:48 -0400

    Added to PlacePoint
    - Made it so PlacePoint visibility is controllable
    - Added size to placepoint

--- src/board/PlacePoint.java
+++ src/board/PlacePoint.java
@@ -4,21 +4,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class PlacePoint extends GameObject {
+	private int size;
+	private boolean isVisible;
 
-	public PlacePoint(int x, int y) {
+	public PlacePoint(int x, int y, int size) {
 		this.x = x;
 		this.y = y;
+		this.size = size;
+		this.isVisible = false;
 	}
 
 	@Override
 	void render(Graphics graphics) {
 		graphics.setColor(Color.RED);
-		graphics.fillRect(x, y, 8, 8);
+		if (this.isVisible) {
+			graphics.fillOval(x - size + 15, y - size + 15, size, size);
+		}
 	}
 
 	@Override
 	void update() {
 
+	}
+
+	// Getters and Setters
+	public void setVisible(boolean b) {
+		this.isVisible = b;
+	}
+
+	public boolean getVisible() {
+		return this.isVisible;
 	}
 
 }
